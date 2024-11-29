10cdd683ec4a14833cbcc4850b22bced72a30319
dermasugita <itsukiisogin@gmail.com>
2024-11-29 23:20:54 +0900

    Disable lualine while cursor at Fern
    This setting is necessary because window tags shown during fern
    open:select are hidden by lualine.

--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
 require('config.lazy')
 require('options')
 require('keymaps')
+require('userCommands')
+require('utils')
+require('lualine').setup {extensions = {'fern'}}
 
 vim.api.nvim_create_autocmd({"VimEnter"}, {
   pattern = {"*"},
@@ -15,4 +18,23 @@ vim.api.nvim_create_autocmd({"VimEnter"}, {
 
 vim.cmd("language en_US")
 
+vim.api.nvim_create_autocmd({"BufEnter"}, {
+  pattern = {'*'},
+  callback = function()
+    local bufname = vim.api.nvim_buf_get_name(0)
+    if (string.startswith(bufname, 'fern')) then
+      require('lualine').hide()
+    end
+  end,
+  nested = true,
+})
+vim.api.nvim_create_autocmd({"BufLeave"}, {
+  pattern = {'*'},
+  callback = function()
+    local bufname = vim.api.nvim_buf_get_name(0)
+    if (string.startswith(bufname, 'fern')) then
+      require('lualine').hide({unhide=true})
+    end
+  end,
+  nested = true,
 })
