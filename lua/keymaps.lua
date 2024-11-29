9db40f84176dfdbaaba77f8977cdd9546663fa6d
dermasugita <itsukiisogin@gmail.com>
2024-11-29 23:07:06 +0900

    Defined keymap for trigger lazygit
    attached <leader>g

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,3 +8,17 @@ vim.keymap.set('i', 'jj', '<ESC>')
 
 -- <Option-n>
 vim.keymap.set('t', '<leader>j', '<C-\\><C-n>')
+
+-- <Leader>g
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  direction = "float",
+  hidden=true,
+})
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+
