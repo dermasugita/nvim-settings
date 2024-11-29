1545c901f2907a4898a498e5ead84ee67e9d50b5
dermasugita <itsukiisogin@gmail.com>
2024-11-29 18:37:59 +0900

    Remove previous term keybinds and set <leader>j for nmode change

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,11 +5,6 @@ vim.keymap.set('i', 'jj', '<ESC>')
 
 
 -- Terminal Window Change
--- <Option-k>
-vim.keymap.set('t', '˚', '<C-\\><C-n><C-w>k')
-
--- <Option-h>
-vim.keymap.set('t', '˙', '<C-\\><C-n><C-w>h')
 
 -- <Option-n>
-vim.keymap.set('t', '˜', '<C-\\><C-n>')
+vim.keymap.set('t', '<leader>j', '<C-\\><C-n>')
